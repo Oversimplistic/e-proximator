4b936afaad6c8ba11749443483b3881878f93da6
Leo Good <leoagood1@gmail.com>
2026-08-04 14:55:21 +0100

    Added pytest for operators

---
+++ tests/operators_test.py
@@ -0,0 +1,60 @@
+import pytest
+from eproximator import run_approximator
+import math
+
+@pytest.fixture
+def e():
+    return math.e
+
+@pytest.fixture
+def e_squared():
+    return math.e**2
+
+@pytest.fixture
+def e_to_the_e():
+    return math.e**math.e
+
+@pytest.fixture
+def e_root_e():
+    return math.e ** (1/math.e)
+
+@pytest.fixture
+def arbitrary_natural_log():
+    return math.log(20, math.e)
+
+
+def test_addition(e):
+    approximation, b, c = run_approximator(e, 0, 0)
+    assert approximation == e
+    assert b == ["+"]
+
+def test_subtraction(e):
+    approximation, b, c = run_approximator(-e, 0, 0)
+    assert approximation == -e
+    assert b == ["-"]
+
+def test_multiplication(e):
+    approximation, b, c = run_approximator(e, 1, 0)
+    assert approximation == e
+    assert b == ["*"]
+
+def test_division(e):
+    approximation, b, c = run_approximator(1, e, 0)
+    assert approximation == 1
+    assert b == ["/"]
+
+def test_power(e_to_the_e, e):
+    approximation, b, c = run_approximator(e_to_the_e, e, 0)
+    assert approximation == e_to_the_e
+    assert b == ["^e"]
+
+
+def test_root(e_root_e, e):
+    approximation, b, c = run_approximator(e_root_e, e, 0)
+    assert approximation == e_root_e
+    assert b == ["^1/e"]
+
+def test_log(arbitrary_natural_log):
+    approximation, b, c = run_approximator(arbitrary_natural_log, 20, 0)
+    assert approximation == arbitrary_natural_log
+    assert b == ["ln"]

d84107d4d984b106034079f5013cab6118c37c80
Leo Good <leoagood1@gmail.com>
2026-08-19 21:34:40 +0100

    Fixed hill-climbing tests

--- tests/test_hill_climbing.py
+++ tests/test_hill_climbing.py
@@ -1,5 +1,5 @@
 import pytest
-from algorithms.eproximator import run_hill_climber
+from algorithms.hill_climbing_algorithm import run_hill_climber
 import math
 
 @pytest.fixture
@@ -24,37 +24,37 @@ def arbitrary_natural_log():
 
 
 def test_addition(e):
-    approximation, b, c = run_hill_climber(e, 0, 0)
+    path, approximation, steps, error = run_hill_climber(e, 0, 0)
     assert approximation == e
-    assert b == ["+"]
+    assert path == ["+"]
 
 def test_subtraction(e):
-    approximation, b, c = run_hill_climber(-e, 0, 0)
+    path, approximation, steps, error = run_hill_climber(-e, 0, 0)
     assert approximation == -e
-    assert b == ["-"]
+    assert path == ["-"]
 
 def test_multiplication(e):
-    approximation, b, c = run_hill_climber(e, 1, 0)
+    path, approximation, steps, error = run_hill_climber(e, 1, 0)
     assert approximation == e
-    assert b == ["*"]
+    assert path == ["*"]
 
 def test_division(e):
-    approximation, b, c = run_hill_climber(1, e, 0)
+    path, approximation, steps, error = run_hill_climber(1, e, 0)
     assert approximation == 1
-    assert b == ["/"]
+    assert path == ["/"]
 
 def test_power(e_to_the_e, e):
-    approximation, b, c = run_hill_climber(e_to_the_e, e, 0)
+    path, approximation, steps, error = run_hill_climber(e_to_the_e, e, 0)
     assert approximation == e_to_the_e
-    assert b == ["^e"]
+    assert path == ["^e"]
 
 
 def test_root(e_root_e, e):
-    approximation, b, c = run_hill_climber(e_root_e, e, 0)
+    path, approximation, steps, error = run_hill_climber(e_root_e, e, 0)
     assert approximation == e_root_e
-    assert b == ["^1/e"]
+    assert path == ["^1/e"]
 
 def test_log(arbitrary_natural_log):
-    approximation, b, c = run_hill_climber(arbitrary_natural_log, 20, 0)
+    path, approximation, steps, error = run_hill_climber(arbitrary_natural_log, 20, 0)
     assert approximation == arbitrary_natural_log
-    assert b == ["ln"]
+    assert path == ["ln"]

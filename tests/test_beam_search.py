9a94f47652f4d17bd890c5178c5ff5923ecb28c6
Leo Good <leoagood1@gmail.com>
2026-08-20 10:16:50 +0100

    Fixed test_beam_search.py

--- tests/test_beam_search.py
+++ tests/test_beam_search.py
@@ -24,37 +24,37 @@ def arbitrary_natural_log():
 
 
 def test_addition(e):
-    path, approximation, error = beamSearch(e, 0)
+    path, approximation, depth, error = beamSearch(e, 0)
     assert approximation == e
     assert path == ["+"]
 
 def test_subtraction(e):
-    path, approximation, error = beamSearch(-e, 0)
+    path, approximation, depth, error = beamSearch(-e, 0)
     assert approximation == -e
     assert path == ["-"]
 
 def test_multiplication(e):
-    path, approximation, error = beamSearch(e, 1)
+    path, approximation, depth, error = beamSearch(e, 1)
     assert approximation == e
     assert path == ["*"]
 
 def test_division(e):
-    path, approximation, error = beamSearch(1, e)
+    path, approximation, depth, error = beamSearch(1, e)
     assert approximation == 1
     assert path == ["/"]
 
 def test_power(e_to_the_e, e):
-    path, approximation, error = beamSearch(e_to_the_e, e)
+    path, approximation, depth, error = beamSearch(e_to_the_e, e)
     assert approximation == e_to_the_e
     assert path == ["^e"]
 
 
 def test_root(e_root_e, e):
-    path, approximation, error = beamSearch(e_root_e, e)
+    path, approximation, depth, error = beamSearch(e_root_e, e)
     assert approximation == e_root_e
     assert path == ["^1/e"]
 
 def test_log(arbitrary_natural_log):
-    path, approximation, error = beamSearch(arbitrary_natural_log, 20)
+    path, approximation, depth, error = beamSearch(arbitrary_natural_log, 20)
     assert approximation == arbitrary_natural_log
     assert path == ["ln"]
